a1365b172b28e524fe048fab19dde24e0f4e7173
Andy Blight <andy.blight@gmail.com>
2015-05-17 16:49:48 +0100

    Added str method and tests

--- src/CardGame/__init__.py
+++ src/CardGame/__init__.py
@@ -4,14 +4,53 @@ Created on 17 May 2015
 @author: andy
 """
 
+from enum import IntEnum
+
+
+class Suit(IntEnum):
+    """ Suit of the Card. """
+    Undefined = 0
+    Hearts = 1
+    Diamonds = 2
+    Clubs = 3
+    Spades = 4
+    Red = 5
+    Black = 6
+
+
+class Rank(IntEnum):
+    """ Ranks of the Card. """
+    Undefined = 0
+    Ace = 1
+    Two = 2
+    Three = 3
+    Four = 4
+    Five = 5
+    Six = 6
+    Seven = 7
+    Eight = 8
+    Nine = 9
+    Ten = 10
+    Jack = 11
+    Queen = 12
+    King = 13
+
 
 class Card:
+    """ The Card class implements the basic methods for using a card. """
 
-    def __init__(self, suit=0, rank=0):
+    def __init__(self, suit=Suit.Undefined, rank=Rank.Undefined):
         if ((1 <= suit <= 4) and (1 <= rank <= 13)):
             self.suit = suit
             self.rank = rank
         else:
-            self.suit = 0
-            self.rank = 0
+            self.suit = Suit.Undefined
+            self.rank = Rank.Undefined
             raise ValueError("Parameter out of range")
+
+    def __str__(self):
+        suit_string = self.suit.__str__()
+        rank_string = self.rank.__str__()
+        suit_split = suit_string.partition(".")
+        rank_spilt = rank_string.partition(".")
+        return rank_spilt[2] + " of " + suit_split[2]
--- src/CardGame/unittest.py
+++ src/CardGame/unittest.py
@@ -8,21 +8,34 @@ import unittest
 import CardGame
 
 
-class TestCardClass(unittest.TestCase):
-
-    # Tuple of suit, rank.
-    valid_values = ((1, 1), (2, 4), (3, 11), (4, 13))
-    invalid_values = ((0, 0), (5, 1), (3, 14), (6, 20))
+class TestCard(unittest.TestCase):
 
     def test_init_valid(self):
-        for suit, rank in self.valid_values:
+        valid_values = ((1, 1), (2, 4), (3, 11), (4, 13))
+        for suit, rank in valid_values:
             card1 = CardGame.Card(suit, rank)
-            self.assertEqual(card1.rank, rank)
             self.assertEqual(card1.suit, suit)
+            self.assertEqual(card1.rank, rank)
 
     def test_init_invalid(self):
-        for suit, rank in self.invalid_values:
+        invalid_values = ((0, 0), (5, 1), (3, 14), (6, 20))
+        for suit, rank in invalid_values:
             dummy = self.assertRaises(ValueError, CardGame.Card, suit, rank)
+
+    def test_str(self):
+        valid_values = ((1, 1, "Hearts", "Ace"),
+                        (2, 4, "Diamonds", "Four"),
+                        (3, 10, "Clubs", "Ten"),
+                        (3, 11, "Clubs", "Jack"),
+                        (4, 12, "Spades", "Queen"),
+                        (4, 13, "Spades", "King"))
+        for suit, rank, suit_name, rank_name in valid_values:
+            suit_split = suit_name.partition(".")
+            rank_spilt = rank_name.partition(".")
+            card_string = rank_spilt[2] + " of " + suit_split[2]
+            card1 = CardGame.Card(suit, rank)
+            self.assertEqual(card_string, card1.__str__())
+
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
