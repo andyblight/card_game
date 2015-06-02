4cf09d8eebf8b44f977678c94c97a34a70158365
Andy Blight <andy.blight@gmail.com>
2015-06-02 11:22:05 +0100

    Moved Players110 to CardGame.Players. Added methods for playing in the correct order and changed code to use the new methods.

--- src/CardGame/__init__.py
+++ src/CardGame/__init__.py
@@ -111,28 +111,119 @@ class Player():
 
     def __init__(self, name):
         self.hand = Hand(name)
+        self.is_dealer = False
 
     def clear(self):
-        """Clears the hand of the player."""
+        """Clears the hand and dealer status of the player."""
         self.hand.cards.clear()
+        self.is_dealer = False
+
+
+class Players():
+    """A list of player instances with the ability to add players and to
+    iterate over the list of players in a circular fashion with a nominated
+    player starting first."""
+
+    def __init__(self):
+        self.players = list()
+        self.round_next_player = -1
+        self.round_continuous = False
+        self.round_clockwise = True
+        self.dealer_num = -1
+
+    def __len__(self):
+        return len(self.players)
+
+    def append(self, player):
+        """Appends the player."""
+        self.players.append(player)
+
+    def clear(self):
+        """Clears the list of players."""
+        self.players.clear()
+        self.round_next_player = -1
+        self.round_continuous = False
+        self.round_clockwise = True
+        self.dealer_num = -1
+
+    def reset(self):
+        """Reset each player to the default state."""
+        for player_num in range(len(self.players)):
+            self.players[player_num].reset()
+
+    def get_player(self, player_num):
+        """Returns a copy of the player with player_num."""
+        return self.players[player_num]
+
+    def set_player(self, player_num, new_player):
+        """Overwrites the player with player_num."""
+        if 0 <= player_num <= len(self.players):
+            self.players[player_num] = new_player
+        else:
+            raise IndexError
+
+    def set_player_num_of_dealer(self, dealer_player_num):
+        """Marks the given player as dealer."""
+        for player_num in range(len(self.players)):
+            if player_num == dealer_player_num:
+                self.players[player_num].is_dealer = True
+                self.dealer_num = player_num
+            else:
+                self.players[player_num].is_dealer = False
+
+    def get_player_num_of_dealer(self):
+        """Gets the number of the player marked as dealer."""
+        return self.dealer_num
+
+    def get_player_num_left_of_dealer(self):
+        """Gets the number of the player to the left of the dealer."""
+        player_num = self.dealer_num + 1
+        player_num %= len(self.players)
+        return player_num
+
+    def get_player_num_right_of_dealer(self):
+        """Gets the number of the player to the right of the dealer."""
+        player_num = self.dealer_num - 1
+        player_num %= len(self.players)
+        return player_num
+
+    def start_round(self, start_player_num, continuous=False, clockwise=True):
+        """Sets the starting player number and the type of round.
+        Returns the value of start_player_num if in range, otherwise -1."""
+        if 0 <= start_player_num <= len(self.players):
+            self.round_next_player = start_player_num
+        else:
+            self.round_next_player = -1
+        self.round_continuous = continuous
+        self.round_clockwise = clockwise
+        return self.round_next_player
+
+    def get_next_player_num_for_round(self):
+        """Gets the next player number for the round stopping when dealer has
+        completed their turn.  Returns -1 if the round has finished."""
+        if not self.round_continuous and \
+                self.players[self.round_next_player].is_dealer:
+            self.round_next_player = -1
+        else:
+            if self.round_clockwise:
+                self.round_next_player += 1
+            else:
+                self.round_next_player -= 1
+            self.round_next_player %= len(self.players)
+        return self.round_next_player
 
 
 class Deck(Pile):
-    """class."""
+    """An empty deck with common methods."""
 
     def __init__(self, name="Deck"):
         Pile.__init__(self, name)
-        for suit in Suit:
-            for rank in Rank:
-                if (Suit.Hearts < suit < Suit.Red) \
-                        and (Rank.Ace < rank < Rank.Joker):
-                    self.cards.append(Card(suit, rank))
 
     def shuffle(self):
         """Shuffles the cards in the deck by swapping them."""
         import random
         num_cards = self.count()
-        for dummy in range(1, 2):
+        for dummy in range(1, 3):
             for i in range(num_cards):
                 j = random.randrange(i, num_cards)
                 self.cards[i], self.cards[j] = self.cards[j], self.cards[i]
@@ -140,11 +231,25 @@ class Deck(Pile):
     def deal(self, players, num_cards=999):
         """Deals num_cards to each hand in hands."""
         for dummy in range(0, num_cards):
-            for j in range(len(players)):
+            for player_num in range(len(players)):
                 if self.is_empty():
                     break
+                player = players.get_player(player_num)
                 card = self.pop()
-                players.add_card_for_player(j, card)
+                player.hand.push(card)
+                players.set_player(player_num, player)
+
+
+class Deck52Card(Deck):
+    """The commonly used 52 card deck."""
+
+    def __init__(self, name="Deck"):
+        Deck.__init__(self, name)
+        for suit in Suit:
+            for rank in Rank:
+                if (Suit.Hearts < suit < Suit.Red) \
+                        and (Rank.Ace < rank < Rank.Joker):
+                    self.cards.append(Card(suit, rank))
 
 
 class CardGame():
--- src/CardGame110.py
+++ src/CardGame110.py
@@ -15,70 +15,18 @@ class Player110(CardGame.Player):
     def __init__(self, name=""):
         CardGame.Player.__init__(self, name)
         self.current_score = 0
-        self.is_dealer = False
 
     def reset(self):
         """Resets the player to defaults."""
         self.current_score = 0
-        self.is_dealer = 0
         self.clear()
 
 
-class Players110():
-    """A list of player instances with the ability to add players and to
-    iterate over the list of players in a circular fashion with a nominated
-    player starting first."""
-
-    def __init__(self):
-        self.entry = 0
-        self.players = list()
-
-    def __len__(self):
-        return len(self.players)
-
-    def append(self, player):
-        """Appends the player."""
-        self.players.append(player)
-
-    def clear(self):
-        """Clears the list of players."""
-        self.players.clear()
-
-    def reset(self):
-        """Reset each player to the default state."""
-        for i in range(len(self.players)):
-            self.players[i].reset()
-
-    def get_player(self, player_num):
-        """Returns a copy of the player with player_num."""
-        return self.players[player_num]
-
-    def set_player(self, player_num, new_player):
-        """Overwrites the player with player_num."""
-        self.players[player_num] = new_player
-
-    def add_card_for_player(self, player_num, card):
-        """Add card to player_num hand."""
-        self.players[player_num].hand.add(card)
-
-    def list_hand_for_player(self, player_num):
-        """Print out hand for player_num."""
-        self.players[player_num].hand.list()
-
-    def get_name(self, player_num):
-        """Get name of player. """
-        return self.players[player_num].hand.name
-
-    def is_dealer(self, player_num):
-        """Get value of is_dealer for player. """
-        return self.players[player_num].is_dealer
-
-
-class Deck110(CardGame.Deck):
+class Deck110(CardGame.Deck52Card):
     """The deck for 110 is a normal deck with one joker."""
 
-    def __init__(self, name="Deck"):
-        CardGame.Deck.__init__(self, name)
+    def __init__(self):
+        CardGame.Deck52Card.__init__(self)
         self.cards.append(CardGame.Card(CardGame.Suit.Red,
                                         CardGame.Rank.Joker))
 
@@ -86,20 +34,23 @@ class Deck110(CardGame.Deck):
 class CardGame110():
     """Plays the game of 110."""
 
-    def __init__(self, num_players):
-        # Create and shuffle the deck
+    def __init__(self):
         self.deck = Deck110()
-        self.deck.shuffle()
-        # Create highest score
         self.highest_score = 0
-        # Create the players
-        self.players = Players110()
-        for i in range(num_players):
-            player = Player110("Player" + str(i))
-            player.current_score = 0
-            if i == 0:
-                player.is_dealer = True
-            self.players.append(player)
+        self.players = CardGame.Players()
+
+    def add_players(self):
+        """For now, this is a bodge so just add four players."""
+        self.players.clear()
+        player = Player110("Andy")
+        player.is_dealer = True
+        self.players.append(player)
+        player = Player110("Brian")
+        self.players.append(player)
+        player = Player110("Colin")
+        self.players.append(player)
+        player = Player110("David")
+        self.players.append(player)
 
     def reset_scores(self):
         """ ."""
@@ -109,20 +60,31 @@ class CardGame110():
 
     def deal_hands(self):
         """Deals 5 cards to each player."""
-        print("Dealing hand")
-        self.deck.deal(self.players, 5)
+        num_cards = 5
+        print("Dealing", num_cards, "cards to",
+              len(self.players), "players")
+        self.deck.deal(self.players, num_cards)
 
     def bid(self):
-        """The bidding phase of the game.  Starting with the player to the
-        left of the dealer, each player bids for the right to chose the
-        trump suit.  The bids must be either no bid or a minimum of 15 points
-        and a maximum of 30 points.  The dealer has the bonus of winning the
-        bid with a bid equal to the highest bid."""
-        print("bidding")
-        bidding_player = 0
-        highest_bid = 10
-        highest_bid_player = -1
-        for player_num in range(0, len(self.players)):
+        """The bidding phase of the game.
+        Starting with the player to the left of the dealer, each player bids
+        for the right to chose the trump suit.  The bids must be either no
+        bid or a minimum of 15 points and a maximum of 30 points.  The dealer 
+        has the bonus of winning the bid with a bid equal to the current
+        highest bid.
+        The bidding is complete when either a bid of 30 is called or refused
+        by the dealer or the when the highest, non-maximum bid is refused by
+        all players after the player with the current highest bid.
+        Returns the player number with the winning bid."""
+        print("Bidding round")
+        winning_bid = 10
+        winning_bid_player = -1
+        starting_player_num = self.players.get_player_num_left_of_dealer()
+        player_num = self.players.start_round(starting_player_num, True)
+        while player_num is not -1:
+            if player_num is winning_bid_player:
+                # Gone round with no takers so bidding over
+                break
             player = self.players.get_player(player_num)
             player.hand.list()
             bid_string = input("Enter bid, 0, 15, 20, 25, 30 ")
@@ -132,20 +94,24 @@ class CardGame110():
                 bid_value = -1
             if bid_value >= 15 and bid_value <= 30 and (bid_value % 5) == 0:
                 # Bid in range
-                if bid_value > highest_bid:
-                    highest_bid = bid_value
-                    highest_bid_player = player_num
-                    print(player.hand.name + " takes bid with "
-                          + str(bid_value))
-                if bid_value == highest_bid and \
-                        self.players.is_dealer(player_num):
-                    highest_bid_player = player_num
-                    print("Dealer takes bid with " + bid_value)
+                if bid_value > winning_bid:
+                    winning_bid = bid_value
+                    winning_bid_player = player_num
+                    print(
+                        player.hand.name + " takes bid with " + str(bid_value))
+                if player.is_dealer:
+                    if bid_value == winning_bid:
+                        winning_bid_player = player_num
+                        print("Dealer takes bid with " + bid_value)
+                    if bid_value == 30:
+                        # Maximum bid and player is dealer so bidding over
+                        break
             # Else bid out of range so treat as no bid.
-        if highest_bid == 10:
+            player_num = self.players.get_next_player_num_for_round()
+        if winning_bid == 10:
             print("No bid from anyone.  Deal again.")
-            return False
-        return True
+            winning_bid_player = -1
+        return winning_bid_player
 
     def mark_cards_for_discard(self, player):
         """Mark the cards in the players hand that will be discarded."""
@@ -172,10 +138,11 @@ class CardGame110():
                 discarding = False
         return cards_to_discard
 
-    def exchange_cards(self):
+    def exchange_cards(self, starting_player_num):
         """Each player in turn discards zero or more cards."""
         print("Exchange cards")
-        for player_num in range(0, len(self.players)):
+        player_num = self.players.start_round(starting_player_num)
+        while player_num is not -1:
             player = self.players.get_player(player_num)
             cards_to_discard = self.mark_cards_for_discard(player)
             # Replace them with new cards from the deck.
@@ -184,16 +151,19 @@ class CardGame110():
                     player.hand.cards[card_index] = self.deck.pop()
             # Update the player's cards
             self.players.set_player(player_num, player)
+            player_num = self.players.get_next_player_num_for_round()
 
-    def play_hand(self):
+    def play_hand(self, starting_player_num):
         """ ."""
         print("play hand")
-        for player_num in range(0, len(self.players)):
+        player_num = self.players.start_round(starting_player_num)
+        while player_num is not -1:
             player = self.players.get_player(player_num)
             # Debug
             player.hand.list()
+            player_num = self.players.get_next_player_num_for_round()
 
-    def update_scores(self):
+    def update_scores(self, starting_player_num):
         """ ."""
         print("Updating scores")
         # AJB Bodge
@@ -202,34 +172,46 @@ class CardGame110():
         self.players.set_player(0, player)
         # Update self.highest_score from players individual scores
         self.highest_score = 0
-        for player_num in range(len(self.players)):
+        winning_player = -1
+        player_num = self.players.start_round(starting_player_num)
+        while player_num is not -1:
             player = self.players.get_player(player_num)
-            print(player.hand.name + " has " +
-                  str(player.current_score))
             if self.highest_score < player.current_score:
                 self.highest_score = player.current_score
-
-#    def terminate_game(self):
-#        """ ."""
+            # The first player to score 110 wins the game
+            if self.highest_score >= 110:
+                winning_player = player_num
+                print(
+                    player.hand.name + " has won the game with a score of " +
+                    str(player.current_score))
+                break
+            else:
+                print(player.hand.name + " has a score" +
+                      str(player.current_score))
+            player_num = self.players.get_next_player_num_for_round()
+        return winning_player
 
     def play(self):
         """play a round."""
         play_again = True
         while play_again:
-            self.reset_scores()
             # Zero current scores
-            while self.highest_score < 110:
+            self.reset_scores()
+            winning_player_num = -1
+            while winning_player_num == -1:
                 self.deal_hands()
-                if self.bid():
-                    self.exchange_cards()
-                    self.play_hand()
-                    self.update_scores()
+                starting_player_num = self.bid()
+                if starting_player_num is not -1:
+                    self.exchange_cards(starting_player_num)
+                    self.play_hand(starting_player_num)
+                    winning_player_num = self.update_scores(
+                        starting_player_num)
             input_string = input('Enter y to play again...')
             if input_string != "y":
                 play_again = False
-#           self.terminate_game()
 
 
 if __name__ == '__main__':
-    GAME = CardGame110(4)
+    GAME = CardGame110()
+    GAME.add_players()
     GAME.play()
