cd31290d6efbf1df574d959d46a1e81ac704b5f6
Andy Blight <andy.blight@gmail.com>
2015-08-08 08:43:06 +0100

    Added client.py

---
+++ src/client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+ 
+# Import PySide classes
+import sys
+from PySide.QtCore import *
+from PySide.QtGui import *
+ 
+# Create a Qt application
+app = QApplication(sys.argv)
+# Create a Label and show it
+label = QLabel("<font color=red size=40>Hello World</font>")
+label.show()
+# Enter Qt application main loop
+app.exec_()
+sys.exit()
