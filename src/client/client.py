077700947fac12c43e83a915c440f312bb53b9a5
Andy Blight <andy.blight@gmail.com>
2015-08-09 20:35:53 +0100

    Added menus for app using PySide

--- src/client/client.py
+++ src/client/client.py
@@ -1,15 +1,146 @@
-#!/usr/bin/python3
- 
-# Import PySide classes
-import sys
-from PySide.QtCore import *
-from PySide.QtGui import *
- 
-# Create a Qt application
-app = QApplication(sys.argv)
-# Create a Label and show it
-label = QLabel("<font color=red size=40>Hello World</font>")
-label.show()
-# Enter Qt application main loop
-app.exec_()
-sys.exit()
+#!/usr/bin/env python
+
+############################################################################
+#
+#  Copyright (C) 2004-2005 Trolltech AS. All rights reserved.
+#
+#  This file is part of the example classes of the Qt Toolkit.
+#
+#  This file may be used under the terms of the GNU General Public
+#  License version 2.0 as published by the Free Software Foundation
+#  and appearing in the file LICENSE.GPL included in the packaging of
+#  self file.  Please review the following information to ensure GNU
+#  General Public Licensing requirements will be met:
+#  http://www.trolltech.com/products/qt/opensource.html
+#
+#  If you are unsure which license is appropriate for your use, please
+#  review the following information:
+#  http://www.trolltech.com/products/qt/licensing.html or contact the
+#  sales department at sales@trolltech.com.
+#
+#  This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+#  WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+############################################################################
+
+from PySide import QtCore, QtGui
+
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        widget = QtGui.QWidget()
+        self.setCentralWidget(widget)
+
+        topFiller = QtGui.QWidget()
+        topFiller.setSizePolicy(QtGui.QSizePolicy.Expanding,
+                QtGui.QSizePolicy.Expanding)
+
+        self.infoLabel = QtGui.QLabel(
+                "<i>Choose a menu option, or right-click to invoke a context menu</i>",
+                alignment=QtCore.Qt.AlignCenter)
+        self.infoLabel.setFrameStyle(QtGui.QFrame.StyledPanel | QtGui.QFrame.Sunken)
+
+        bottomFiller = QtGui.QWidget()
+        bottomFiller.setSizePolicy(QtGui.QSizePolicy.Expanding,
+                QtGui.QSizePolicy.Expanding)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.setContentsMargins(5, 5, 5, 5)
+        vbox.addWidget(topFiller)
+        vbox.addWidget(self.infoLabel)
+        vbox.addWidget(bottomFiller)
+        widget.setLayout(vbox)
+
+        self.createActions()
+        self.createMenus()
+
+        message = "A context menu is available by right-clicking"
+        self.statusBar().showMessage(message)
+
+        self.setWindowTitle("Menus")
+        self.setMinimumSize(160,160)
+        self.resize(480,320)
+
+    def contextMenuEvent(self, event):
+        menu = QtGui.QMenu(self)
+        menu.addAction(self.selectGameAct)
+        menu.addAction(self.restartGameAct)
+        menu.exec_(event.globalPos())
+
+    def selectGame(self):
+        self.infoLabel.setText("Invoked <b>Game|Select</b>")
+
+    def restartGame(self):
+        self.infoLabel.setText("Invoked <b>Game|Restart</b>")
+
+    def undo(self):
+        self.infoLabel.setText("Invoked <b>Edit|Undo</b>")
+
+    def redo(self):
+        self.infoLabel.setText("Invoked <b>Edit|Redo</b>")
+
+    def about(self):
+        self.infoLabel.setText("Invoked <b>Help|About</b>")
+        QtGui.QMessageBox.about(self, "About Menu",
+                "The <b>Menu</b> example shows how to create menu-bar menus "
+                "and context menus.")
+
+    def aboutQt(self):
+        self.infoLabel.setText("Invoked <b>Help|About Qt</b>")
+
+    def createActions(self):
+        self.selectGameAct = QtGui.QAction("&Select...", self,
+                shortcut=QtGui.QKeySequence.New,
+                statusTip="Select a game", triggered=self.selectGame)
+
+        self.restartGameAct = QtGui.QAction("&Restart...", self,
+                shortcut=QtGui.QKeySequence.Open,
+                statusTip="Restart the current game", triggered=self.restartGame)
+
+        self.exitAct = QtGui.QAction("E&xit", self, shortcut="Ctrl+Q",
+                statusTip="Exit the application", triggered=self.close)
+
+        self.undoAct = QtGui.QAction("&Undo", self,
+                shortcut=QtGui.QKeySequence.Undo,
+                statusTip="Undo the last operation", triggered=self.undo)
+
+        self.redoAct = QtGui.QAction("&Redo", self,
+                shortcut=QtGui.QKeySequence.Redo,
+                statusTip="Redo the last operation", triggered=self.redo)
+
+        self.aboutAct = QtGui.QAction("&About", self,
+                statusTip="Show the application's About box",
+                triggered=self.about)
+
+        self.aboutQtAct = QtGui.QAction("About &Qt", self,
+                statusTip="Show the Qt library's About box",
+                triggered=self.aboutQt)
+        self.aboutQtAct.triggered.connect(QtGui.qApp.aboutQt)
+
+
+    def createMenus(self):
+        self.fileMenu = self.menuBar().addMenu("&Game")
+        self.fileMenu.addAction(self.selectGameAct)
+        self.fileMenu.addAction(self.restartGameAct)
+        self.fileMenu.addSeparator()
+        self.fileMenu.addAction(self.exitAct)
+
+        self.editMenu = self.menuBar().addMenu("&Edit")
+        self.editMenu.addAction(self.undoAct)
+        self.editMenu.addAction(self.redoAct)
+
+        self.helpMenu = self.menuBar().addMenu("&Help")
+        self.helpMenu.addAction(self.aboutAct)
+        self.helpMenu.addAction(self.aboutQtAct)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QtGui.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
